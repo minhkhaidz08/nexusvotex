cfa63f6520fe81d007fbf28226294f3f01b7c672
minhkhaidz08 <minhkhaidz08@gmail.com>
2026-08-08 17:40:02 +0700

    Delete backend directory

--- backend/src/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { webhookController } = require('../controllers/webhook.controller');
-
-router.post('/thesieure', webhookController.theSieuRe);
-router.get('/thesieure', webhookController.theSieuRe);
-router.post('/payos', webhookController.payos);
-
-module.exports = router;
