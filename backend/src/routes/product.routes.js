d5b43b736f91c764e63db5f8b8e8091ed8fc0466
minhkhaidz08 <minhkhaidz08@gmail.com>
2026-08-08 16:36:02 +0700

    Delete backend directory

--- backend/src/routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { productController } = require('../controllers/product.controller');
-const { authMiddleware } = require('../middleware/auth');
-
-router.get('/', productController.list);
-router.get('/featured', productController.featured);
-router.get('/:id', productController.detail);
-
-module.exports = router;
